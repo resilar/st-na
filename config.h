aadff794a559d26b38ec879a4397b19c9a191937
def <def@huumeet.info>
2025-09-19 18:33:08 +0300

    reduce font pixelsize 16 -> 15

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=15:antialias=true:autohint=true";
 static char *font2[] = {
-	"NotoColorEmoji:pixelsize=16:antialias=true:autohint=true"
+	"NotoColorEmoji:pixelsize=15:antialias=true:autohint=true"
 };
 static int borderpx = 0;
 
